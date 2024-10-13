9180f9737d3c8366759408f6c8dbec6e58445b9e
Codngwthubaid <codngwthubaid@gmail.com>
2024-10-13 13:29:11 +0530

    Start working on destination component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,18 @@
 import './App.css'
 import React from 'react'
 import HeroSection from './components/HeroSection'
+import Destination from './components/Destination'
 
 function App() {
   return (
     <>
-      <main className='container'>
+      <main className='container mx-auto'>
         <article>
           <section>
-            <HeroSection/>
+            <HeroSection />
+          </section>
+          <section>
+            <Destination />
           </section>
         </article>
       </main>
