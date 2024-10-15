244f5b5f2f971f8b0cf82a49438de824ca2f8429
Codngwthubaid <codngwthubaid@gmail.com>
2024-10-15 09:48:08 +0530

    Add the razorpayment gateway functionality

---
+++ src/app/api/Payment/route.js
@@ -0,0 +1,28 @@
+import { NextResponse } from 'next/server'
+import Razorpay from 'razorpay'
+
+const razorpay = new Razorpay({
+    key_id : process.env.RAZORPAY_KEY_ID,
+    Key_Secret : process.env.RAZORPAY_KEY_SECRET
+})
+
+export async function POST() {
+    try {
+        const order = await  razorpay.orders.create({
+            amount: 100 * 100,
+            currency: "INR",
+            receipt: "receipt" + Math.random().toString(36).substring(7) 
+        })
+
+        return NextResponse.json(
+            {orderId : order.id},
+            {status: 200}
+        )
+    } catch (error) {
+        console.log("Error Creating Order : ",error);
+        return NextResponse.json(
+            {error: "Error Creating Order"},
+            {status: 500}
+        )
+    }
+}
---
+++ src/app/pages/PaymentGateway/page.js
@@ -0,0 +1,67 @@
+"use client"
+
+import React, { useState } from 'react'
+import Script from 'next/script'
+
+const Page = () => {
+    const AMOUNT = 100;
+    const [isProcessing, setIsProcessing] = useState(false)
+
+    const handlePayment = async () => {
+        setIsProcessing(true)
+
+        try {
+            const response = await fetch("/api/Payment", { method: "post" })
+            const data = await response.json()
+
+            // Init Razorpay
+            const options = {
+                key: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
+                currency: "INR",
+                amount: AMOUNT * 100,
+                name: "Company Name",
+                description: "Test Transaction",
+                order_id: data.order_id,
+                handler: function (response) {
+                    console.log("Payment Successful : ", response);
+                },
+                prefill: {
+                    name: "Ubaid",
+                    email: "codngwthubaid@gmail.com",
+                    contact: "7535000000"
+                },
+                theme: {
+                    color: "#3399cc"
+                }
+            };
+
+            const windowRazorpay = new window.Razorpay(options)
+            windowRazorpay.open();
+
+        } catch (error) {
+            console.log("Payment failed : ", error);
+        }
+        finally {
+            setIsProcessing(false)
+        }
+    }
+
+    return (
+        <div>
+            <div className='flex justify-center items-center bg-slate-500 flex-col'>
+                <Script src='https://checkout.razorpay.com/v1/checkout.js' />
+                <h1 className='text-2xl font-bold mb-4'></h1>
+                <p className='mb-4'>Amount to pay : {AMOUNT} INR</p>
+                <button
+                    onClick={handlePayment}
+                    disabled={isProcessing}
+                    className='text-white bg-blue-600 hover:bg-blue-800'
+                >
+                    {isProcessing ? "Processing" : "Pay Now"}
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Page
